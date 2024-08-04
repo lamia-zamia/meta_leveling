4ddc1cbd05146e1d5c4e5a5ead08b6cb747451a7
Lamia <xaxnas@gmail.com>
2024-08-04 21:21:41 +0100

    better checks for enemy visibility

--- files/scripts/attach_scripts/enemy_on_death.lua
+++ files/scripts/attach_scripts/enemy_on_death.lua
@@ -1,33 +1,55 @@
+---@type meta_leveling
 local ML = dofile_once("mods/meta_leveling/files/scripts/utilities/meta_leveling.lua")
-local distance_x = 428
-local distance_y = 243
+local waters = dofile_once("mods/meta_leveling/files/scripts/compatibility/water_list.lua")
+local T = GameTextGetTranslatedOrNot
 
-local function get_distance(died_entity)
-	local d_x, d_y = EntityGetTransform(died_entity)
-	local p_x, p_y = EntityGetTransform(ML.utils:get_player_id())
-	local x = math.abs(d_x - p_x)
-	local y = math.abs(d_y - p_y)
-	return {x = x, y = y}
+local function not_visible(entity)
+	local offset = 20
+	local cam_x, cam_y, cam_w, cam_h = GameGetCameraBounds()
+	local ent_x, ent_y = EntityGetTransform(entity)
+	if ent_x + offset < cam_x or ent_x - offset > cam_x + cam_w then return true end
+	if ent_y + offset < cam_y or ent_y - offset > cam_y + cam_h then return true end
+	local fog = GameGetFogOfWarBilinear(ent_x, ent_y)
+	if fog > 110 then return true end
+	return false
 end
 
+local function damage_done_by_water(damage_message)
+	for _, water in ipairs(waters) do
+		local text = GameTextGet("$damage_frommaterial", T(water))
+		if text == damage_message then
+			return true
+		end
+	end
+	return false
+end
+
+---@param damage_type_bit_field damage_type_bit_field
+---@param damage_message damage_message
+---@param entity_thats_responsible entity_id
 function death(damage_type_bit_field, damage_message, entity_thats_responsible, drop_items)
 	local died_entity = GetUpdatedEntityID()
 	local exp = ML:convert_max_hp_to_exp(died_entity)
 	local message = nil
-	local died_name = GameTextGetTranslatedOrNot(EntityGetName(died_entity))
+	local died_name = T(EntityGetName(died_entity))
 
-	if EntityHasTag(entity_thats_responsible, "player_unit") then
-		message = "Killed " .. died_name .. ", gain xp: " .. exp
+	if EntityHasTag(entity_thats_responsible, "player_unit") then -- killed by player
+		message = T("$ml_killed") .. " : " .. died_name .. ", " .. T("$ml_gained_xp") .. ": " .. exp
 		ML:add_exp(exp)
 	else
-		local distance = get_distance(died_entity)
-		if distance.x > distance_x or distance.y > distance_y then return end
+		if not_visible(died_entity) then return end
 		local responsible_name = EntityGetName(entity_thats_responsible)
-		if responsible_name then
-			message = GamePrint("Died from enemy: " .. died_name .. ", resposible: " .. GameTextGetTranslatedOrNot(responsible_name))
-		else
-			local cause = GameTextGetTranslatedOrNot(damage_message)
-			message = "Died: " .. died_name .. ", cause: " .. cause .. ", resposible: unknown"
+		if responsible_name then -- killed by someone else
+			-- message = GamePrint("Died from enemy: " .. died_name .. ", resposible: " .. GameTextGetTranslatedOrNot(responsible_name))
+		else               -- environmental kills
+			local cause = T(damage_message)
+			local multiplier = 0.5
+			if damage_message == "$damage_water" then multiplier = multiplier + 0.5 end
+			if damage_type_bit_field == 32 and damage_done_by_water(damage_message) then multiplier = multiplier + 0.5 end
+
+			message = T("$ml_died") .. ": " .. died_name .. ", " .. T("$ml_cause") .. ": "
+				.. cause .. ", " .. T("$ml_gained_xp") .. ": " .. exp * multiplier
+			ML:add_exp(exp * multiplier)
 		end
 	end
 	if message and ML.utils:get_mod_setting_boolean("session_exp_log") then GamePrint(message) end
