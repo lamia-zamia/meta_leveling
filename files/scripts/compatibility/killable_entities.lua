f1f23e95493d1ec5c8d2859823901cdbbd84733b
Lamia <xaxnas@gmail.com>
2024-08-12 15:33:22 +0100

    cabbage, leggy

--- files/scripts/compatibility/killable_entities.lua
+++ files/scripts/compatibility/killable_entities.lua
@@ -10,6 +10,7 @@ local bosses = {
 	"data/entities/animals/boss_dragon.xml",
 	"data/entities/animals/boss_fish/fish_giga.xml",
 	"data/entities/animals/maggot_tiny/maggot_tiny.xml",
+	"data/entities/animals/boss_limbs/boss_limbs.xml",
 }
 ML.utils:merge_tables(entities, bosses)
 
@@ -24,12 +25,15 @@ local wtf_are_these = {
 	"data/entities/animals/shooterflower.xml",
 	"data/entities/animals/worm.xml",
 	"data/entities/animals/worm_big.xml",
+	"data/entities/animals/chest_leggy.xml",
 	-- "data/entities/animals/lukki/lukki_creepy_long.xml",
 	-- "data/entities/animals/lukki/lukki_creepy.xml",
 	-- "data/entities/animals/lukki/lukki_dark.xml",
 	-- "data/entities/animals/lukki/lukki_longleg.xml",
 	-- "data/entities/animals/lukki/lukki_tiny.xml",
 	-- "data/entities/animals/lukki/lukki.xml",
+	-- "data/entities/props/temple_statue_01.xml",
+	-- "data/entities/props/temple_statue_02.xml"
 }
 ML.utils:merge_tables(entities, wtf_are_these)
 
