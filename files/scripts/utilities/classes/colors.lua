45bcad71928d558be45b56f0d21863dd7439adb0
Lamia <xaxnas@gmail.com>
2024-08-14 22:23:50 +0100

    anno

--- files/scripts/utilities/classes/colors.lua
+++ files/scripts/utilities/classes/colors.lua
@@ -1,21 +1,37 @@
----@class colors
----@field colors color_converter
+---@class colors_hue
+---@class colors_saturation
+---@class colors_value
+
+---@class (exact) colors
+---@field private converter color_converter
 local colors = {
 	converter = dofile_once("mods/meta_leveling/files/scripts/utilities/lib/color_converter.lua")
 }
 
+---@private
 function colors:mult255(r, g, b)
     return r * 255, g * 255, b * 255
 end
 
+---@private
 function colors:div255(r, g, b)
     return r / 255, g / 255, b / 255
 end
 
+---converts noita-rgb to 255hsv
+---@param r number
+---@param g number
+---@param b number
+---@return colors_hue, colors_saturation, colors_value
 function colors:rgb2hsv(r, g, b)
     return self.converter:rgb2hsv(self:mult255(r, g, b))
 end
 
+---converts 255hsv to noita-rgb
+---@param h colors_hue
+---@param s colors_saturation
+---@param v colors_value
+---@return number red, number green, number blue
 function colors:hsv2rgb(h, s, v)
     return self:div255(self.converter:hsv2rgb(h, s, v))
 end
