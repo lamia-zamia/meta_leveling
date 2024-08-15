7a2aec062e46d6f2fe6b3572eddef6e4d23e901b
Lamia <xaxnas@gmail.com>
2024-08-15 14:48:30 +0100

    anno, generalize

--- files/scripts/attach_scripts/enemy_on_death.lua
+++ files/scripts/attach_scripts/enemy_on_death.lua
@@ -59,7 +59,7 @@ function death(damage_type_bit_field, damage_message, entity_thats_responsible, 
 			ML.exp:add(exp)
 		end
 	end
-	if message and ML.utils:get_mod_setting_boolean("session_exp_popup") then ML.font:popup(died_entity,
+	if message and ML.utils:get_mod_setting_boolean("session_exp_popup") then ML.font:popup_exp(died_entity,
 			ML.exp:floor(exp), exp) end
 	if message and ML.utils:get_mod_setting_boolean("session_exp_log") then GamePrint(message) end
 end
--- files/scripts/utilities/classes/font.lua
+++ files/scripts/utilities/classes/font.lua
@@ -10,50 +10,65 @@ local font = {
 	LuaComponent = {
 		script_source_file = "mods/meta_leveling/files/scripts/attach_scripts/popup_exp.lua",
 	},
-	r = "0",
-	g = "0",
-	b = "0"
 }
 
-function font:get_color()
-	self.r = string.format("%.2f", ML.utils:get_mod_setting_number("exp_bar_red"))
-	self.g = string.format("%.2f", ML.utils:get_mod_setting_number("exp_bar_green"))
-	self.b = string.format("%.2f", ML.utils:get_mod_setting_number("exp_bar_blue"))
+---returns string of rgb or exp color if none
+---@param r? number|string
+---@param g? number|string
+---@param b? number|string
+---@return string r, string g, string b
+function font:get_color(r, g, b)
+	if r then r = tostring(r) else r = string.format("%.2f", ML.utils:get_mod_setting_number("exp_bar_red")) end
+	if g then g = tostring(g) else g = string.format("%.2f", ML.utils:get_mod_setting_number("exp_bar_green")) end
+	if b then b = tostring(b) else b = string.format("%.2f", ML.utils:get_mod_setting_number("exp_bar_blue")) end
+	return r, g, b
 end
 
-function font:get_path()
-	self:get_color()
-	local path = self.path .. self.r .. self.g .. self.b .. ".xml"
-	if ModDoesFileExist(path) then
-		return path
-	else
-		font:generate(path)
-		return path
-	end
-end
-
-function font:generate(path)
-	path = path or self:get_path()
+---generate virtual font
+---@private
+---@param r string
+---@param g string
+---@param b string
+---@param path string
+function font:generate(r, g, b, path)
 	local xml = ML.nxml.parse(ModTextFileGetContent(self.source))
-	xml.attr.color_r = self.r
-	xml.attr.color_g = self.g
-	xml.attr.color_b = self.b
+	xml.attr.color_r = r
+	xml.attr.color_g = g
+	xml.attr.color_b = b
 	xml.attr.color_a = "1"
 	ModTextFileSetContent(path, tostring(xml))
 end
 
-function font:popup(source_entity, text, scale)
-	if scale < 1 then
-		scale = 0.5
+---returns path to virtual colored font and generate if it doesn't exist
+---@param r string
+---@param g string
+---@param b string
+---@return string path
+function font:get_path(r, g, b)
+	local path = self.path .. r .. g .. b .. ".xml"
+	if ModDoesFileExist(path) then
+		return path
 	else
-		scale = scale ^ 0.25 * 0.5
+		font:generate(r, g, b, path)
+		return path
 	end
+end
+
+---popup text on entity
+---@param source_entity entity_id
+---@param text string
+---@param r string
+---@param g string
+---@param b string
+---@param scale number
+function font:popup(source_entity, text, scale, r, g, b)
+	scale = scale or 1
 	local x, y = EntityGetFirstHitboxCenter(source_entity)
 	local offset = #text * self.font_size * scale
 	if not x then return end
 	entity = EntityCreateNew("ml_exp_popup")
 	EntityAddComponent2(entity, "SpriteComponent", {
-		image_file = self:get_path(),
+		image_file = self:get_path(r, g, b),
 		is_text_sprite = true,
 		text = text,
 		z_index = -2,
@@ -62,6 +77,19 @@ function font:popup(source_entity, text, scale)
 	EntityAddComponent2(entity, "LifetimeComponent", self.LifetimeComponent)
 	EntityAddComponent2(entity, "LuaComponent", self.LuaComponent)
 	EntitySetTransform(entity, x - offset, y - 7, 0, scale, scale)
+end
+
+---popup exp on entity
+---@param source_entity entity_id
+---@param text string
+---@param scale number
+function font:popup_exp(source_entity, text, scale)
+	if scale < 1 then
+		scale = 0.5
+	else
+		scale = scale ^ 0.25 * 0.5
+	end
+	self:popup(source_entity, text, scale, self:get_color())
 end
 
 return font
