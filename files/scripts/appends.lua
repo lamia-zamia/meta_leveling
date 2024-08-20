ed754190dc31920a4f7d9be19712b9dc858fb3a6
Lamia <xaxnas@gmail.com>
2024-08-20 20:14:21 +0100

    unused

--- files/scripts/appends.lua
+++ files/scripts/appends.lua
@@ -1,3 +1,2 @@
 dofile_once("mods/meta_leveling/files/scripts/appends/append_translations.lua")
 dofile_once("mods/meta_leveling/files/scripts/appends/rewards.lua")
--- dofile_once("mods/meta_leveling/files/scripts/appends/add_death_script.lua")
