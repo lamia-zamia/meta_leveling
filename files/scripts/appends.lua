5f96f98ca69ddbbee3e4d5d13686cd2113a582e7
Lamia <xaxnas@gmail.com>
2024-08-21 11:13:58 +0100

    altar quests

--- files/scripts/appends.lua
+++ files/scripts/appends.lua
@@ -1,2 +1,3 @@
 dofile_once("mods/meta_leveling/files/scripts/appends/append_translations.lua")
 dofile_once("mods/meta_leveling/files/scripts/appends/rewards.lua")
+dofile_once("mods/meta_leveling/files/scripts/appends/mountain_altar.lua")
