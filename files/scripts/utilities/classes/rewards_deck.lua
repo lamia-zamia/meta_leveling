2769a601b71504879ebf3540e0b27ede2b358dff
Lamia <xaxnas@gmail.com>
2024-08-25 17:16:49 +0100

    refactored deck

--- files/scripts/utilities/classes/rewards_deck.lua
+++ files/scripts/utilities/classes/rewards_deck.lua
@@ -1,9 +1,9 @@
----@class ml_reward_definition_list
----@field [number] ml_reward
+---@class ml_reward_id
 
 ---@class (exact) ml_single_reward_data:ml_reward
+---@field id ml_reward_id The id of the reward
+---@field group_id ml_reward_id The id of the group if part of a group
 ---@field fn function function to run on pick
----@field group_id string id of group if part of group
 ---@field ui_icon string path to icon
 ---@field probability number|fun():number should be between 0 and 1
 ---@field max number max number of reward that you can pick
@@ -11,7 +11,7 @@
 ---@field pick_count number
 
 ---@class (exact) group_data
----@field rewards table<[number], [string]> list of rewards in group
+---@field rewards table<[number], [ml_reward_id]> list of rewards in group
 ---@field picked boolean if something from group was picked
 
 ---@class (exact) groups_data
@@ -28,7 +28,7 @@
 ---@field private max_probability number max probability for single reward
 ---@field private min_probability number min probability for single reward
 ---@field private list table table of rewards id
----@field private distance number mimimum distance between rewards
+---@field private distance number minimum distance between rewards
 ---@field reroll_count number
 local rewards_deck = {
 	reward_data = {},
@@ -41,19 +41,21 @@ local rewards_deck = {
 	reroll_count = 1,
 }
 
----default function if function was not found
+---Logs an error message with an optional custom text.
+---@param text? string
 ---@private
-function rewards_deck:function_not_found()
-	print("error!")
+function rewards_deck:function_error(text)
+	print("\27[31m[Meta Leveling Error]\27[0m")
+	print("[Meta Leveling]: error - " .. (text or "unknown error"))
 end
 
----add single reward to list
+---Adds a single reward to the reward list.
 ---@param table ml_reward
 function rewards_deck:add_reward(table)
 	self.reward_definition_list[#self.reward_definition_list + 1] = table
 end
 
----add array of rewards to list
+---Adds an array of rewards to the reward list.
 ---@param table ml_rewards
 function rewards_deck:add_rewards(table)
 	for _, reward in ipairs(table) do
@@ -61,17 +63,17 @@ function rewards_deck:add_rewards(table)
 	end
 end
 
----function to validate deck
+---Validates the reward data and group data for consistency.
 ---@private
 function rewards_deck:ValidateData()
 	local reward_count = 0
 	for id, reward in pairs(self.reward_data) do
 		if reward.limit_before and not self.reward_data[reward.limit_before] then
-			print("[Meta Leveling]: error - limit before is invalid in reward: " .. id)
+			self:function_error("invalid limit_before in reward: " .. id)
 			self.reward_data[id].limit_before = nil
 		end
 		if reward.custom_check and type(reward.custom_check()) ~= "boolean" then
-			print("[Meta Leveling]: error - custom check is invalid in reward: " .. id)
+			self:function_error("invalid custom_check in reward: " .. id)
 			self.reward_data[id].custom_check = nil
 		end
 		reward_count = reward_count + 1
@@ -81,67 +83,85 @@ function rewards_deck:ValidateData()
 	for _, _ in pairs(self.groups_data) do
 		group_count = group_count + 1
 	end
-	print("[Meta Leveling]: loaded " .. reward_count .. " rewards, ".. group_count .. " unique rewards group")
+	print("[Meta Leveling]: loaded " .. reward_count .. " rewards, " .. group_count .. " unique rewards group")
 end
 
----function to gather data from list
+---Gathers reward data from a predefined list.
 function rewards_deck:GatherData()
-	self.reward_definition_list = dofile_once("mods/meta_leveling/files/scripts/session_level/rewards/level_up_rewards.lua")
+	self.reward_definition_list = dofile_once(
+		"mods/meta_leveling/files/scripts/session_level/rewards/level_up_rewards.lua")
 	dofile_once("mods/meta_leveling/files/scripts/session_level/rewards/rewards_append.lua")
+
 	for _, reward in ipairs(self.reward_definition_list) do
 		if self.reward_data[reward.id] then
-			print("[Meta Leveling]: warning - duplicate reward id " .. reward.id)
-		end
-		self.reward_data[reward.id] = {
-			id = reward.id,
-			group_id = reward.group_id or reward.id,
-			ui_name = reward.ui_name,
-			description = reward.description or nil,
-			description_var = reward.description_var or nil,
-			description2 = reward.description2 or nil,
-			description2_var = reward.description2_var or nil,
-			ui_icon = reward.ui_icon or self.default_icon,
-			probability = self:set_probability(reward.probability),
-			max = reward.max or 1280,
-			fn = reward.fn or self.function_not_found,
-			pick_count = self:get_specific_reward_pickup_amount(reward.id),
-			limit_before = reward.limit_before or nil,
-			custom_check = reward.custom_check or nil,
-			sound = reward.sound or ML.const.sounds.perk,
-			no_sound = reward.no_sound,
-			min_level = reward.min_level or 1
-		}
-		local group_id = self.reward_data[reward.id].group_id
-		if self.groups_data[group_id] then
-			self.groups_data[group_id].rewards[#self.groups_data[group_id].rewards + 1] = reward.id
+			self:function_error("duplicate reward id " .. reward.id)
 		else
-			self.groups_data[group_id] = {
-				rewards = { reward.id },
-				picked = false
-			}
+			self:initialize_reward_data(reward)
 		end
-		if self.reward_data[reward.id].pick_count > 0 then self.groups_data[group_id].picked = true end
 	end
+
 	self:ValidateData()
 end
 
----get normalized probability
+---Initializes the reward data and group data based on the reward definition.
+---@private
+---@param reward ml_reward
+function rewards_deck:initialize_reward_data(reward)
+	local reward_entry = {
+		id = reward.id,
+		group_id = reward.group_id or reward.id,
+		ui_name = reward.ui_name,
+		description = reward.description or nil,
+		description_var = reward.description_var or nil,
+		description2 = reward.description2 or nil,
+		description2_var = reward.description2_var or nil,
+		ui_icon = reward.ui_icon or self.default_icon,
+		probability = self:set_probability(reward.probability),
+		max = reward.max or 1280,
+		fn = reward.fn or self.function_error,
+		pick_count = self:get_specific_reward_pickup_amount(reward.id --[[@as ml_reward_id]]),
+		limit_before = reward.limit_before or nil,
+		custom_check = reward.custom_check or nil,
+		sound = reward.sound or ML.const.sounds.perk,
+		no_sound = reward.no_sound,
+		min_level = reward.min_level or 1
+	}
+
+	self.reward_data[reward.id] = reward_entry
+	self:initialize_group_data(reward_entry)
+end
+
+---Initializes the group data for a given reward.
+---@private
+---@param reward_entry ml_single_reward_data
+function rewards_deck:initialize_group_data(reward_entry)
+	local group_id = reward_entry.group_id
+
+	if not self.groups_data[group_id] then
+		self.groups_data[group_id] = { rewards = {}, picked = false }
+	end
+
+	self.groups_data[group_id].rewards[#self.groups_data[group_id].rewards + 1] = reward_entry.id
+
+	if reward_entry.pick_count > 0 then
+		self.groups_data[group_id].picked = true
+	end
+end
+
+---Sets the probability of a reward, normalizing it if necessary.
 ---@private
 ---@param probability number|fun():number
 ---@return number|fun():number
 function rewards_deck:set_probability(probability)
-	probability = probability or 1
 	if type(probability) == "number" then return self:probability_normalize(probability) end
-	return probability
+	return probability or 1
 end
 
----normalize probability
+---Normalizes the probability of a reward.
 ---@param probability number
 ---@return number
 function rewards_deck:probability_normalize(probability)
-	probability = math.min(self.max_probability, probability)
-	probability = math.max(probability, self.min_probability + 0.01)
-	return probability
+	return math.max(self.min_probability + 0.01, math.min(self.max_probability, probability))
 end
 
 ---get normalized probability
@@ -153,26 +173,31 @@ function rewards_deck:get_probability(probability)
 	return self:probability_normalize(probability())
 end
 
----shuffle table
+---Shuffles the list of rewards.
 ---@private
 function rewards_deck:shuffle()
-	local iterations = #self.list
-	local j
-	for i = iterations, 2, -1 do
-		j = Random(1, i)
+	for i = #self.list, 2, -1 do
+		local j = Random(1, i)
 		self.list[i], self.list[j] = self.list[j], self.list[i]
 	end
 end
 
+---Adds a reward to the list a specified number of times.
+---@private
+---@param reward_id ml_reward_id
+---@param amount number
 function rewards_deck:add_to_list(reward_id, amount)
 	for _ = 1, amount do
 		self.list[#self.list + 1] = reward_id
 	end
 end
 
+---Checks if a reward meets the criteria to be added to the list.
+---@private
+---@param reward ml_single_reward_data
+---@return boolean
 function rewards_deck:checks_before_add(reward)
-	local pass = true
-	pass = pass and reward.pick_count < reward.max
+	local pass = reward.pick_count < reward.max
 	pass = pass and ML:get_level() >= reward.min_level
 	if reward.limit_before then
 		pass = pass and self.reward_data[reward.limit_before].pick_count >= self.reward_data[reward.limit_before].max
@@ -183,7 +208,7 @@ function rewards_deck:checks_before_add(reward)
 	return pass
 end
 
----gather list
+---Gathers rewards from the data and populates the list.
 ---@private
 function rewards_deck:get_from_list()
 	self.distance = self:get_draw_amount() * 4
@@ -194,28 +219,28 @@ function rewards_deck:get_from_list()
 	end
 end
 
----ensure rewards are not spawned too close to each other
+---Ensures rewards are not spawned too close to each other.
 ---@private
 function rewards_deck:ensure_distance()
 	local temp_new = {}
-	local count = #self.list
+	-- local count = #self.list
 
-	for i = 1, count do
+	for i = 1, #self.list do
 		if not self.list[i] then goto continue end
-		if i + self.distance > count then break end
+		if i + self.distance > #self.list then break end
 		for j = i + 1, i + self.distance do
 			if self.reward_data[self.list[i]].group_id == self.reward_data[self.list[j]].group_id then
 				goto continue
 			end
 		end
-		temp_new[#temp_new+1] = self.list[i]
+		temp_new[#temp_new + 1] = self.list[i]
 		::continue::
 	end
 
 	self.list = temp_new
 end
 
----Generate reward list
+---Refreshes the order of rewards in the list.
 ---@private
 function rewards_deck:refresh_reward_order()
 	SetRandomSeed(1, 2)
@@ -224,20 +249,20 @@ function rewards_deck:refresh_reward_order()
 	self:ensure_distance()
 end
 
----returns how many to draw
+---Returns the number of rewards to draw.
 ---@return number
 function rewards_deck:get_draw_amount()
 	return ML.utils:get_global_number(ML.const.globals.draw_amount, 0) + 3
 end
 
----return current draw index
+---Returns the current draw index.
 ---@private
 ---@return number
 function rewards_deck:get_draw_index()
 	return ML.utils:get_global_number(ML.const.globals.draw_index, 1)
 end
 
----return next valid draw index
+---Returns the next valid draw index.
 ---@private
 ---@param current_index number
 ---@return number
@@ -246,14 +271,14 @@ function rewards_deck:get_next_draw_index(current_index)
 		self:refresh_reward_order()
 		current_index = 1
 	end
-	while self.list[current_index] == nil or self.list[current_index] == "" do
+	while self.list[current_index] == nil do
 		current_index = current_index + 1
 		if current_index > 10000 then break end
 	end
 	return current_index
 end
 
----set next draw index
+---Sets the next draw index.
 ---@private
 function rewards_deck:set_draw_index()
 	local index = self:get_draw_index()
@@ -263,8 +288,8 @@ function rewards_deck:set_draw_index()
 	ML.utils:set_global_number(ML.const.globals.draw_index, index)
 end
 
----Return next reward
----@return table reward_ids
+---Draws the next rewards from the list.
+---@return table<[number], ml_reward_id> reward_ids
 function rewards_deck:draw_reward()
 	self:refresh_reward_order()
 	local draw_index = self:get_draw_index()
@@ -277,51 +302,63 @@ function rewards_deck:draw_reward()
 	return draw_list
 end
 
----get reward picked up count
----@param reward_id string
+---Returns the pickup count for a specific reward.
+---@param reward_id ml_reward_id
 ---@return number pickup_count
 function rewards_deck:get_specific_reward_pickup_amount(reward_id)
 	return ML.utils:get_global_number(ML.const.globals_prefix .. reward_id .. "_PICKUP_COUNT", 0)
 end
 
----add reward pick up count
+---Increments the pickup count for a specific reward.
 ---@private
----@param reward_id string
+---@param reward_id ml_reward_id
 function rewards_deck:add_specific_reward_pickup_amount(reward_id)
 	self.reward_data[reward_id].pick_count = self.reward_data[reward_id].pick_count + 1
 	self.groups_data[self.reward_data[reward_id].group_id].picked = true
-	ML.utils:set_global_number(ML.const.globals_prefix .. reward_id .. "_PICKUP_COUNT", self.reward_data[reward_id].pick_count)
+	ML.utils:set_global_number(ML.const.globals_prefix .. reward_id .. "_PICKUP_COUNT",
+		self.reward_data[reward_id].pick_count)
 end
 
+---Skips the current reward and advances the draw index.
 function rewards_deck:skip_reward()
 	self:set_draw_index()
 end
 
+---Plays the sound associated with a drawn reward.
+---@param draw_id ml_reward_id
 function rewards_deck:play_sound(draw_id)
-	if self.reward_data[draw_id].no_sound then return end
-	GamePlaySound(self.reward_data[draw_id].sound.bank, self.reward_data[draw_id].sound.event, ML.player.x, ML.player.y)
+	local reward = self.reward_data[draw_id]
+	if not reward.no_sound then
+		GamePlaySound(reward.sound.bank, reward.sound.event, ML.player.x, ML.player.y)
+	end
 end
 
----do stuff after pickup
----@param draw_id string
+---Executes actions when a reward is picked.
+---@param draw_id ml_reward_id
 function rewards_deck:pick_reward(draw_id)
-	rewards_deck:set_draw_index()
-
-	rewards_deck:play_sound(draw_id)
-	rewards_deck.reward_data[draw_id].fn()
-
-	rewards_deck:add_specific_reward_pickup_amount(draw_id)
+	local success, err = pcall(rewards_deck.reward_data[draw_id].fn)
+	if success then
+		rewards_deck:set_draw_index()
+		rewards_deck:play_sound(draw_id)
+		rewards_deck:add_specific_reward_pickup_amount(draw_id)
+	else
+		self:function_error("function of " .. draw_id .. " throw an error, error: " .. err)
+	end
 end
 
+---Retrieves the current reroll count.
 function rewards_deck:get_reroll_count()
 	self.reroll_count = ML.utils:get_global_number(ML.const.globals.reroll_count, 1)
 end
 
+---Adds to the reroll count.
+---@param count number
 function rewards_deck:add_reroll(count)
 	self.reroll_count = self.reroll_count + count
 	ML.utils:set_global_number(ML.const.globals.reroll_count, self.reroll_count)
 end
 
+---Performs a reroll, decrementing the reroll count and setting the next draw index.
 function rewards_deck:reroll()
 	self.reroll_count = self.reroll_count - 1
 	ML.utils:set_global_number(ML.const.globals.reroll_count, self.reroll_count)
