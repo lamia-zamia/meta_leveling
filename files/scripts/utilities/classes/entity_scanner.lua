9f04c3063196e562ad50776ceffc5a9bf03f7488
Lamia <xaxnas@gmail.com>
2024-08-28 16:48:19 +0100

    ?

--- files/scripts/utilities/classes/entity_scanner.lua
+++ files/scripts/utilities/classes/entity_scanner.lua
@@ -7,7 +7,7 @@ local entity_scanner = {
 ---@param entity entity_id
 ---@private
 function entity_scanner:add_lua_component_if_none(entity)
-	local components = EntityGetComponentIncludingDisabled(entity, "LuaComponent") or {}
+	local components = EntityGetComponent(entity, "LuaComponent") or {}
 	for _, component in ipairs(components) do
 		if ComponentGetValue2(component, "script_death") == "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua" then return end
 	end
@@ -15,7 +15,6 @@ function entity_scanner:add_lua_component_if_none(entity)
 		execute_every_n_frame= -1,
 		script_death = "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua",
 		script_damage_received = "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua",
-		-- remove_after_executed=true
 	})
 end
 
