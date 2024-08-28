d21731525e92c6a18079c2da1d80f50237e11777
Lamia <xaxnas@gmail.com>
2024-08-28 17:16:33 +0100

    cache for scanned entities

--- files/scripts/utilities/classes/entity_scanner.lua
+++ files/scripts/utilities/classes/entity_scanner.lua
@@ -1,7 +1,9 @@
 ---@class (exact) ml_entity_scanner
 ---@field private tags table
+---@field private processed_entities table
 local entity_scanner = {
-	tags = {"enemy", "nest", "homing_target"}
+	tags = { "enemy", "nest", "homing_target" },
+	processed_entities = setmetatable({}, { __mode = "k" }),
 }
 
 ---@param entity entity_id
@@ -9,22 +11,32 @@ local entity_scanner = {
 function entity_scanner:add_lua_component_if_none(entity)
 	local components = EntityGetComponent(entity, "LuaComponent") or {}
 	for _, component in ipairs(components) do
-		if ComponentGetValue2(component, "script_death") == "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua" then return end
+		if ComponentGetValue2(component, "script_death") == "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua" then
+			self.processed_entities[entity] = true
+			return
+		end
 	end
 	EntityAddComponent2(entity, "LuaComponent", {
-		execute_every_n_frame= -1,
+		execute_every_n_frame = -1,
 		script_death = "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua",
 		script_damage_received = "mods/meta_leveling/files/scripts/attach_scripts/enemy_on_death.lua",
 	})
+	self.processed_entities[entity] = true
 end
 
 ---scan entities and add death script if none
 function entity_scanner:check_entities()
+	local unprocessed_entities = {}
 	for _, tag in ipairs(self.tags) do
 		local entities = EntityGetWithTag(tag)
 		for _, entity in ipairs(entities) do
-			self:add_lua_component_if_none(entity)
+			if not self.processed_entities[entity] then
+				unprocessed_entities[entity] = true
+			end
 		end
+	end
+	for entity, _ in pairs(unprocessed_entities) do
+		self:add_lua_component_if_none(entity)
 	end
 end
 
