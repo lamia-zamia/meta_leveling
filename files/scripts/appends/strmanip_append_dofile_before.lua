540454f433e922ddd259c59f449af11727f56a51
Lamia <xaxnas@gmail.com>
2024-09-02 15:53:02 +0100

    sunquest

--- files/scripts/appends/strmanip_append_dofile_before.lua
+++ files/scripts/appends/strmanip_append_dofile_before.lua
@@ -4,6 +4,12 @@ local SM = dofile_once("mods/meta_leveling/files/scripts/utilities/lib/stringman
 local append_list = {
 	["data/scripts/buildings/sun/spot_1_finish.lua"] =
 	"mods/meta_leveling/files/scripts/appends/dofile_before/sunquest/spot_1_finish.lua",
+	["data/scripts/buildings/sun/spot_2.lua"] =
+	"mods/meta_leveling/files/scripts/appends/dofile_before/sunquest/spot_1_finish.lua",
+	["data/scripts/buildings/sun/spot_3.lua"] =
+	"mods/meta_leveling/files/scripts/appends/dofile_before/sunquest/spot_1_finish.lua",
+	["data/scripts/buildings/sun/spot_4.lua"] =
+	"mods/meta_leveling/files/scripts/appends/dofile_before/sunquest/spot_4.lua",
 	["data/entities/animals/boss_centipede/ending/sampo_start_ending_sequence.lua"] =
 	"mods/meta_leveling/files/scripts/appends/dofile_before/sampo_start_ending_sequence.lua",
 	["data/scripts/magic/altar_tablet_magic.lua"] =
