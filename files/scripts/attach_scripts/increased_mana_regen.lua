0052622a688a6d3a0134d935afcacd79a47cc582
Lamia <xaxnas@gmail.com>
2024-10-12 18:11:10 +0100

    buggg

--- files/scripts/attach_scripts/increased_mana_regen.lua
+++ files/scripts/attach_scripts/increased_mana_regen.lua
@@ -7,4 +7,4 @@ if not wand_id then return end
 
 local wand = ezwand(wand_id)
 wand.mana = wand.mana +
-	wand.manaChargeSpeed / 60 * MLP.get:global_number(MLP.const.globals.permanent_concentrated_mana, 0)
+	wand.manaChargeSpeed / 60 * meta.get:global_number(meta.const.globals.permanent_concentrated_mana, 0)
