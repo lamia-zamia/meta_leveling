648d4e4a39afe8b7c4bde3d2e1c7a30254830bf9
Lamia <xaxnas@gmail.com>
2024-10-18 18:29:43 +0100

    nerf mudmans

--- files/scripts/compatibility/experience_custom.lua
+++ files/scripts/compatibility/experience_custom.lua
@@ -1,5 +1,6 @@
 ---@enum CustomExpEntities
 CustomExpEntities = {
-	["data/entities/animals/ghostling_body.xml"] = 5
+	["data/entities/animals/ghostling_body.xml"] = 5,
+	["data/entities/animals/mudman_friendly.xml"] = 0.5
 }
 
