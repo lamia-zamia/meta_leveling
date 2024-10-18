7214b9f28d036498066f3e3e850d4037af0e934a
Lamia <xaxnas@gmail.com>
2024-10-18 22:40:12 +0100

    refactored death script, added meta point on boss kill

--- files/scripts/attach_scripts/enemy_on_death.lua
+++ files/scripts/attach_scripts/enemy_on_death.lua
@@ -1,5 +1,5 @@
-dofile_once("mods/meta_leveling/files/scripts/compatibility/experience_custom.lua") --- importing CustomExpEntities
-dofile_once("mods/meta_leveling/files/scripts/compatibility/water_list.lua") --- importing WaterMaterials
+dofile_once("mods/meta_leveling/files/scripts/compatibility/experience_custom.lua")  --- importing CustomExpEntities
+dofile_once("mods/meta_leveling/files/scripts/compatibility/water_list.lua")         --- importing WaterMaterials
 local MLP = dofile_once("mods/meta_leveling/files/scripts/meta_leveling_public.lua") --- @type MetaLevelingPublic
 local T = GameTextGetTranslatedOrNot
 
@@ -32,6 +32,73 @@ local function damage_done_by_water(damage_message)
 	return false
 end
 
+-- local function get_base_experience
+
+--- Determines if was hurt by player
+--- @return boolean
+--- @nodiscard
+local function is_player_kill()
+	local current_frame = GameGetFrameNum()
+	local last_damage_by_player = GetValueInteger("ML_damaged_by_player", -900)
+	return (current_frame - last_damage_by_player) < 180
+end
+
+--- Process friendly fire
+--- @param multiplier number
+--- @param died_name string
+--- @param responsible_name string
+--- @return number, string
+local function betray_exp(multiplier, died_name, responsible_name)
+	local betrayal_multiplier = MLP.get:global_number(MLP.const.globals.exp_betray, 0)
+	if betrayal_multiplier == 0 then return 0, "" end
+
+	multiplier = multiplier * betrayal_multiplier
+	local message = T("$ml_died") .. ": " .. died_name .. ", " .. T("$ml_cause") ..
+		": " .. responsible_name .. ", " .. T("$ml_gained_xp") .. ": "
+
+	return multiplier, message
+end
+
+--- Process environmental kills
+--- @param multiplier number
+--- @param died_name string
+--- @param damage_message string
+--- @param damage_type_bit_field number
+--- @return number, string
+local function trick_exp(multiplier, died_name, damage_message, damage_type_bit_field)
+	local cause = T(damage_message)
+	local trick_multiplier = 0.25 + MLP.get:global_number(MLP.const.globals.exp_trick, 0)
+
+	-- Increase multiplier if water is involved in the kill
+	if damage_message == "$damage_water" or (damage_type_bit_field == 32 and damage_done_by_water(damage_message)) then
+		trick_multiplier = trick_multiplier + 0.5
+	end
+
+	multiplier = multiplier * trick_multiplier
+	local message = T("$ml_died") .. ": " .. died_name .. ", " .. T("$ml_cause") .. ": "
+		.. cause .. ", " .. T("$ml_gained_xp") .. ": "
+
+	return multiplier, message
+end
+
+--- Process experience value
+--- @param multiplier number
+--- @param died_entity entity_id
+--- @return number
+local function process_boss(multiplier, died_entity)
+	-- Check for boss tag to double the experience
+	if MLP.get:entity_has_tag(died_entity, "boss") then 
+		multiplier = multiplier * 2 
+		MLP.points:add_meta_points(1)
+	end
+
+	-- Blessed beast integration
+	if MLP.get:entity_has_tag(died_entity, "blessed_beast") then multiplier = multiplier * 1.5 end
+	if MLP.get:entity_has_tag(died_entity, "blessed_miniboss") then multiplier = multiplier * 2 end
+
+	return multiplier
+end
+
 --- @type script_damage_received
 local script_damage_received = function(damage, message, entity_thats_responsible, is_fatal, projectile_thats_responsible)
 	local parent = EntityGetRootEntity(entity_thats_responsible)
@@ -48,54 +115,35 @@ local script_death = function(damage_type_bit_field, damage_message, entity_that
 	-- Return if entity belongs to the player's herd
 	if MLP.get:is_player_herd(died_entity) then return end
 
-	-- Calculate base experience from entity's max HP or using custom exp mapping
-	local exp = CustomExpEntities[EntityGetFilename(died_entity)] or MLP.exp:convert_max_hp_to_exp(died_entity)
+	local multiplier = 1
+	local message = nil
 	local died_name = T(EntityGetName(died_entity))
 
-	-- Check for boss tag to double the experience
-	if MLP.get:entity_has_tag(died_entity, "boss") then exp = exp * 2 end
-
-	-- Blessed beast integration
-	if MLP.get:entity_has_tag(died_entity, "blessed_beast") then exp = exp * 1.5 end
-	if MLP.get:entity_has_tag(died_entity, "blessed_miniboss") then exp = exp * 2 end
-
-	local current_frame = GameGetFrameNum()
-	local last_damage_by_player = GetValueInteger("ML_damaged_by_player", -900)
-	local message = nil
-
-	-- ######################### player kills ##########################
-	if current_frame - last_damage_by_player < 180 then
-		if damage_type_bit_field == 1 then exp = exp * 2 end -- Kicks
+	if is_player_kill() then
+		-- Player kill
+		if damage_type_bit_field == 1 then multiplier = multiplier * 2 end -- Kicks
 		message = T("$ml_killed") .. " : " .. died_name .. ", " .. T("$ml_gained_xp") .. ": "
 	else
 		-- Return if entity is off-screen
 		if not is_entity_visible(died_entity) then return end
 
-		local responsible_name = EntityGetName(entity_thats_responsible)
+		local responsible_name = EntityGetName(EntityGetRootEntity(entity_thats_responsible))
 
-		-- ######################### killed by someone ##########################
 		if responsible_name then
-			local betrayal_multiplier = MLP.get:global_number(MLP.const.globals.exp_betray, 0)
-			if betrayal_multiplier == 0 then return end
-
-			exp = exp * betrayal_multiplier
-			message = T("$ml_died") .. ": " .. died_name .. ", " .. T("$ml_cause") ..
-				": " .. responsible_name .. ", " .. T("$ml_gained_xp") .. ": "
+			-- Killed by another entity
+			multiplier, message = betray_exp(multiplier, died_name, responsible_name)
+			if multiplier <= 0 then return end
 		else
-			-- ######################### trick kills ##########################
-			local cause = T(damage_message)
-			local multiplier = 0.25 + MLP.get:global_number(MLP.const.globals.exp_trick, 0)
-
-			-- Increase multiplier if water is involved in the kill
-			if damage_message == "$damage_water" or (damage_type_bit_field == 32 and damage_done_by_water(damage_message)) then
-				multiplier = multiplier + 0.5
-			end
-
-			exp = exp * multiplier
-			message = T("$ml_died") .. ": " .. died_name .. ", " .. T("$ml_cause") .. ": "
-				.. cause .. ", " .. T("$ml_gained_xp") .. ": "
+			-- Environmental kills
+			multiplier, message = trick_exp(multiplier, died_name, damage_message, damage_type_bit_field)
 		end
 	end
-	MLP:AddExpGlobal(exp, died_entity, message)
+
+	multiplier = process_boss(multiplier, died_entity)
+
+	-- Calculate base experience from entity's max HP or using custom exp mapping
+	local exp = CustomExpEntities[EntityGetFilename(died_entity)] or MLP.exp:convert_max_hp_to_exp(died_entity)
+
+	MLP:AddExpGlobal(exp * multiplier, died_entity, message)
 end
 death = script_death
