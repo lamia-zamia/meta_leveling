39bcc77dec57da7844e9583a1e43372af53c2595
Lamia <xaxnas@gmail.com>
2024-10-19 18:20:44 +0100

    more nerf

--- files/scripts/compatibility/experience_custom.lua
+++ files/scripts/compatibility/experience_custom.lua
@@ -1,6 +1,7 @@
 ---@enum CustomExpEntities
 CustomExpEntities = {
 	["data/entities/animals/ghostling_body.xml"] = 5,
-	["data/entities/animals/mudman_friendly.xml"] = 0.5
+	["data/entities/animals/mudman_friendly.xml"] = 0.5,
+	["mods/new_enemies/data/entities/animals/twig.xml"] = 1,
 }
 
