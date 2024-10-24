768bea24fa9ad3b2f070d0a88e01bbe879ab1fbe
Lamia <xaxnas@gmail.com>
2024-10-24 16:21:10 +0100

    nerf chest reward abuse

--- files/scripts/rewards/add_reward_example.lua
+++ files/scripts/rewards/add_reward_example.lua
@@ -8,12 +8,34 @@ local reward = {
 	ui_icon = "mods/meta_leveling/files/gfx/rewards/spell_refresh.xml",
 	sound = const.sounds.refresh,
 	probability = 0.6,
-	fn = function()
-		GameRegenItemActionsInPlayer(ML.player.id)
-	end
+	fn = function() GameRegenItemActionsInPlayer(ML.player.id) end,
 }
 
 rewards_deck:add_reward(reward)
+
+local function is_near_mountain_altar()
+	local mountain = EntityGetWithTag("ending_sampo_spot_mountain")[1]
+	if mountain and ML.player.y < -1000 then
+		local x = EntityGetTransform(mountain)
+		if ML.player.x > x - 150 and ML.player.x < x + 150 then return true end
+	end
+	return false
+end
+
+local function spawn_chest(chance)
+	if is_near_mountain_altar() then
+		ML.utils:load_entity_to_player("data/entities/items/pickup/chest_leggy.xml")
+		return
+	end
+
+	ML.utils:random_seed()
+	local rnd = Random(1, 2000)
+	if rnd >= chance then
+		ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random_super.xml")
+	else
+		ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random.xml")
+	end
+end
 
 ---@type ml_rewards
 local rewards = {
@@ -25,15 +47,7 @@ local rewards = {
 		ui_icon = "mods/meta_leveling/files/gfx/rewards/spawn_chest.png",
 		probability = 0.6,
 		max = 3,
-		fn = function()
-			ML.utils:random_seed()
-			local rnd = Random(1, 2000)
-			if (rnd >= 1999) then
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random_super.xml")
-			else
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random.xml")
-			end
-		end
+		fn = function() spawn_chest(1999) end,
 	},
 	{
 		id = "pickup_spawn_chest2",
@@ -46,15 +60,7 @@ local rewards = {
 		border_color = rewards_deck.borders.uncommon,
 		max = 3,
 		limit_before = "pickup_spawn_chest1",
-		fn = function()
-			ML.utils:random_seed()
-			local rnd = Random(1, 2000)
-			if (rnd >= 1900) then
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random_super.xml")
-			else
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random.xml")
-			end
-		end
+		fn = function() spawn_chest(1900) end,
 	},
 	{
 		id = "pickup_spawn_chest3",
@@ -67,15 +73,7 @@ local rewards = {
 		border_color = rewards_deck.borders.rare,
 		max = 3,
 		limit_before = "pickup_spawn_chest2",
-		fn = function()
-			ML.utils:random_seed()
-			local rnd = Random(1, 2000)
-			if (rnd >= 1800) then
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random_super.xml")
-			else
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random.xml")
-			end
-		end
+		fn = function() spawn_chest(1800) end,
 	},
 	{
 		id = "pickup_spawn_chest4",
@@ -88,15 +86,7 @@ local rewards = {
 		border_color = rewards_deck.borders.epic,
 		max = 3,
 		limit_before = "pickup_spawn_chest3",
-		fn = function()
-			ML.utils:random_seed()
-			local rnd = Random(1, 2000)
-			if (rnd >= 1700) then
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random_super.xml")
-			else
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random.xml")
-			end
-		end
+		fn = function() spawn_chest(1700) end,
 	},
 	{
 		id = "pickup_spawn_chest5",
@@ -108,15 +98,7 @@ local rewards = {
 		probability = 0.3,
 		border_color = rewards_deck.borders.legendary,
 		limit_before = "pickup_spawn_chest4",
-		fn = function()
-			ML.utils:random_seed()
-			local rnd = Random(1, 2000)
-			if (rnd >= 1500) then
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random_super.xml")
-			else
-				ML.utils:load_entity_to_player("data/entities/items/pickup/chest_random.xml")
-			end
-		end
+		fn = function() spawn_chest(1500) end,
 	},
 	{
 		id = "pickup_spawn_chest_mimic",
@@ -128,10 +110,8 @@ local rewards = {
 		max = 1,
 		probability = 0.5,
 		limit_before = "pickup_spawn_chest1",
-		fn = function()
-			ML.utils:load_entity_to_player("data/entities/items/pickup/chest_leggy.xml")
-		end
-	}
+		fn = function() ML.utils:load_entity_to_player("data/entities/items/pickup/chest_leggy.xml") end,
+	},
 }
 
 rewards_deck:add_rewards(rewards)
