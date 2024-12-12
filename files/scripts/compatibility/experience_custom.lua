a354916f5261c28cac34bdd11e2cf93fe4f21698
Lamia <xaxnas@gmail.com>
2024-12-12 12:34:44 +0000

    NE's wizard den boss

--- files/scripts/compatibility/experience_custom.lua
+++ files/scripts/compatibility/experience_custom.lua
@@ -3,5 +3,5 @@ CustomExpEntities = {
 	["data/entities/animals/ghostling_body.xml"] = 5,
 	["data/entities/animals/mudman_friendly.xml"] = 0.5,
 	["mods/new_enemies/data/entities/animals/twig.xml"] = 1,
+	["data/entities/animals/doom_bringer.xml"] = 50,
 }
-
