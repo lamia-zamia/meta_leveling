bdcb21c022dfddddf1fdbb599e5a81a38550dd80
Lamia <xaxnas@gmail.com>
2025-04-21 17:03:02 +0100

    fix NE twig

--- files/scripts/compatibility/experience_custom.lua
+++ files/scripts/compatibility/experience_custom.lua
@@ -2,6 +2,6 @@
 CustomExpEntities = {
 	["data/entities/animals/ghostling_body.xml"] = 5,
 	["data/entities/animals/mudman_friendly.xml"] = 0.1,
-	["mods/new_enemies/data/entities/animals/twig.xml"] = 0.5,
+	["data/entities/animals/twig.xml"] = 0.5,
 	["data/entities/animals/doom_bringer.xml"] = 50,
 }
