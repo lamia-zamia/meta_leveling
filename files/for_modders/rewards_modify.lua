a5379d144a6c65f9b38bd1f462c14b3fe817a4d7
Lamia <xaxnas@gmail.com>
2025-09-19 13:19:53 +0100

    docs

--- files/for_modders/rewards_modify.lua
+++ files/for_modders/rewards_modify.lua
@@ -1,3 +1,6 @@
 --[[
-here goes documentation, maybe later
+local rewards_deck = dofile_once("mods/meta_leveling/files/scripts/classes/private/rewards_deck.lua") ---@type rewards_deck
+for _, reward in ipairs(rewards_deck.reward_definition_list) do
+	-- something
+end
 ]]
