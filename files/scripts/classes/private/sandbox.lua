b48396fc3432dcc06aa65b93b622d4b08ad5abcb
Lamia <xaxnas@gmail.com>
2025-11-13 17:34:28 +0000

    update sandbox

--- files/scripts/classes/private/sandbox.lua
+++ files/scripts/classes/private/sandbox.lua
@@ -1,18 +1,26 @@
---- @class ML_sandbox
+---@class ML_sandbox
 local sandbox = {
 	old = {},
+	dofile = {},
 }
 
 function sandbox:start_sandbox()
 	for key, value in pairs(_G) do
 		self.old[key] = value
 	end
-	dofile_once = dofile
+	self.dofile = {}
+	for k, v in pairs(__loadonce) do
+		self.dofile[k] = v
+	end
 end
 
 function sandbox:end_sandbox()
 	for key, value in pairs(_G) do
 		if self.old[key] ~= value then _G[key] = self.old[key] end
+	end
+	__loadonce = {} ---@diagnostic disable-line: name-style-check
+	for k, v in pairs(self.dofile) do
+		__loadonce[k] = v
 	end
 end
 
